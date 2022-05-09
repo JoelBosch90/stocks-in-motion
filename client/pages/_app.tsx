23c5df822e0182cbccdd266fc4a491935f80ef8f
Joël Bosch <yopc@joelbosch.nl>
2022-05-09 10:32:55 +0200

    Initial about page setup with Next

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,14 @@
+/**
+ *  This adds a global component to the application that can be used to declare
+ *  properties that will be shared by all pages in the application. For example,
+ *  it makes sure that all pages have access to global styling.
+ */
+
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
