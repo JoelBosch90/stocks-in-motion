786094e63cd72dc5c3e8256019023f8764454610
Joël Bosch <yopc@joelbosch.nl>
2022-05-15 14:46:25 +0200

    Added few more stocks to the slider example

--- client/pages/stocks.tsx
+++ client/pages/stocks.tsx
@@ -15,7 +15,7 @@ const Stocks : NextPage = () => {
   const router = useRouter()
 
   // Create some temporary example elements.
-  const cards = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG', 'TSLA', 'BRK.B', 'JNJ', 'UNH', 'FD', 'NVDA', 'XOM', 'PG', 'JPM', 'V', 'CVX', 'HD', 'PFE', 'MA', 'ABBV']
+  const cards = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG', 'TSLA', 'BRK.B', 'JNJ', 'UNH', 'FD', 'NVDA', 'XOM', 'PG', 'JPM', 'V', 'CVX', 'HD', 'PFE', 'MA', 'ABBV', 'KO', 'BAC', 'AVGO', 'PEP', 'LLY']
     .map((name:string) : JSX.Element => <Card name={name} key={name} onClick={() => router.push(`/stocks/${name}`)} />)
 
   return (
