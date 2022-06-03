844ca085026a94b140a1109abdaa3525c317bd03
Mythje <mail@rubenbosch.com>
2022-06-03 21:20:39 +0200

    Rename solution and adding some boilerplate for a DataAccesLayer

---
+++ DataAccesLayer/Models/Stock.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace DataAccesLayer.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ DataAccesLayer/StocksContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccesLayer
+{
+    public class StocksContext : DbContext 
+    {
+        // public DbSet<Stock> Stocks { get; set; }
+        // public DbSet<Exchange> Exchanges { get; set; }
+
+
+    }
+}
