d034b0e8d98069b6f849a67f7a0806666a8714e3
Joël Bosch <yopc@joelbosch.nl>
2022-07-30 12:03:29 +0200

    Trying to connect to the database, but not sure if I'm on the right track here...

---
+++ DataAccessLayer/Migrations/20220730095118_CreateDatabase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StocksAPIs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Added = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StocksAPIs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Added = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Ticker = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    SourceId = table.Column<long>(type: "bigint", nullable: false),
+                    Moment = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stocks_StocksAPIs_SourceId",
+                        column: x => x.SourceId,
+                        principalTable: "StocksAPIs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stocks_SourceId",
+                table: "Stocks",
+                column: "SourceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stocks");
+
+            migrationBuilder.DropTable(
+                name: "StocksAPIs");
+        }
+    }
+}
--- DataAccessLayer/Models/Stock.cs
+++ DataAccessLayer/Models/Stock.cs
@@ -1,11 +1,19 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLayer.Models
 {
     public class Stock
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
+        public long Id { get; set; }
+        public DateTime Added { get; set; }
+        [StringLength(255)]
+        public string? Name { get; set; }
+        [StringLength(5)]
+        public string Ticker { get; set; }
+        public string Currency { get; set; }
+        public int Price { get; set; }
+        public StocksAPI Source { get; set; }
+        public DateTime Moment { get; set; }
     }
 }
