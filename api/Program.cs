d20a0094764f9acf4ddbd43dfdf18ecab4c480aa
Joël Bosch <yopc@joelbosch.nl>
2022-12-03 17:21:48 +0100

    Added database migrations on startup, I think

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,4 @@
+using DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,3 +24,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Run pending database migrations.
+using IServiceScope scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+using StocksContext context = scope.ServiceProvider.GetRequiredService<StocksContext>();
+context.Database.Migrate();
